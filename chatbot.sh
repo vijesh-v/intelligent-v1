965439defe5331d4bebc6c8de1aa117327576e5a
Vijesh V Nair <59731317+vijesh-v@users.noreply.github.com>
2020-08-13 18:58:00 +0530

    Update chatbot.sh

--- chatbot.sh
+++ chatbot.sh
@@ -1,6 +1,11 @@
+command="{{ trigger.body.text }}"
+echo "Executing Command : $command"
+
 ./oc login --token=Ns9Q0zEUDSvDs-Guyp60uUIoEvNXUmOBMHeQv1IpSAE --server=https://c100-e.eu-gb.containers.cloud.ibm.com:30450
 
 ./oc project i2devops
-output=`./oc {{ trigger.body.text }} | awk '/Running/ {print}'`
+output=`./oc get pods | awk '/Running/ {print}' | awk 'BEGIN {printf("%-10s %-10s %-10s %-10s %-20s\n" ,"AGE", "READY", "STATUS", "RESTARTS", "NAME")} {printf("%-10s %-10s %-10s %-10s %-20s\n", $5, $2, $3, $4, $1)}'`
 
-curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$output\"}" https://hooks.slack.com/services/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
+echo $output
+
+curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$output\"}" https://hooks.slack.com/services/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
