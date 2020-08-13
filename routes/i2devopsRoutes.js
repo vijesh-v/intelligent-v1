39235e0cd2d22a39d5746ae6e3dc06bad2edce90
Vijesh V Nair <59731317+vijesh-v@users.noreply.github.com>
2020-08-13 14:16:04 +0530

    Update i2devopsRoutes.js

--- routes/i2devopsRoutes.js
+++ routes/i2devopsRoutes.js
@@ -21,6 +21,7 @@ router.post('/api/v1/i2devops', async (req, res) => {
           throwHttpErrors : false
         }
 		const stackstormresponse = await got.post("https://34.69.73.187/api/v1/webhooks/pod_restart", options);
+		stackstormresponse.challenge = req.body.challenge;
 		res.status(stackstormresponse.statusCode).send(stackstormresponse.body);
 	}
 	catch (err) {
