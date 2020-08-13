31c44bc8c0e48902feebbfa543d123da15943c92
Vijesh V Nair <59731317+vijesh-v@users.noreply.github.com>
2020-08-13 19:29:14 +0530

    Update chatbot.sh

--- chatbot.sh
+++ chatbot.sh
@@ -2,16 +2,16 @@ command="$1"
 whoami
 echo "Executing Command : $command"
 
-if [ $command -eq *"get pods"*  ]
+if [[ "$command" -eq *"get pods"* ]]
 then
-        echo Please Install Docker and try again.
+        ./oc login --token=Ns9Q0zEUDSvDs-Guyp60uUIoEvNXUmOBMHeQv1IpSAE --server=https://c100-e.eu-gb.containers.cloud.ibm.com:30450
+
+        ./oc project i2devops
+        output=`./oc get pods | awk '/Running/ {print}' | awk 'BEGIN {printf("%-10s %-10s %-10s %-10s %-20s\n" ,"AGE", "READY", "STATUS", "RESTARTS", "NAME")} {printf("%-10s %-10s %-10s %-10s %-20s\n", $5, $2, $3, $4, $1)}''`
+
+        echo $output
+
+        curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$output\"}" https://hooks.slack.com/services/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
 fi
 
-./oc login --token=Ns9Q0zEUDSvDs-Guyp60uUIoEvNXUmOBMHeQv1IpSAE --server=https://c100-e.eu-gb.containers.cloud.ibm.com:30450
 
-./oc project i2devops
-output=`./oc get pods | awk '/Running/ {print}' | awk 'BEGIN {printf("%-10s %-10s %-10s %-10s %-20s\n" ,"AGE", "READY", "STATUS", "RESTARTS", "NAME")} {printf("%-10s %-10s %-10s %-10s %-20s\n", $5, $2, $3, $4, $1)}''`
-
-echo $output
-
-curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$output\"}" https://hooks.slack.com/services/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
