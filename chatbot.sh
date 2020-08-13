565dd606e48680dea3845674227a55a4e06f0b0f
Vijesh V Nair <59731317+vijesh-v@users.noreply.github.com>
2020-08-13 16:42:13 +0530

    Update chatbot.sh

--- chatbot.sh
+++ chatbot.sh
@@ -1,6 +1,6 @@
 ./oc login --token=Ns9Q0zEUDSvDs-Guyp60uUIoEvNXUmOBMHeQv1IpSAE --server=https://c100-e.eu-gb.containers.cloud.ibm.com:30450
 
 ./oc project i2devops
-output=`./oc get pods | awk '/Running/ {print}'`
+output=`./oc {{ trigger.body.text }} | awk '/Running/ {print}'`
 
 curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$output\"}" https://hooks.slack.com/services/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
