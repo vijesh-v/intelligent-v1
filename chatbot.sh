001d7770592096d2950b777ceccc0a59b6460741
Vijesh V Nair <59731317+vijesh-v@users.noreply.github.com>
2020-08-13 19:47:03 +0530

    Update chatbot.sh

--- chatbot.sh
+++ chatbot.sh
@@ -2,16 +2,15 @@ command="$1"
 
 echo "Executing Command : $command"
 
-if [[ "$command" == "get pods*" ]]
+if [ "$command" == 'get pods' ]
 then
+        echo "Executing OC Commands"
         ./oc login --token=Ns9Q0zEUDSvDs-Guyp60uUIoEvNXUmOBMHeQv1IpSAE --server=https://c100-e.eu-gb.containers.cloud.ibm.com:30450
 
         ./oc project i2devops
-        output=`./oc get pods | awk '/Running/ {print}' | awk 'BEGIN {printf("%-10s %-10s %-10s %-10s %-20s\n" ,"AGE", "READY", "STATUS", "RESTARTS", "NAME")} {printf("%-10s %-10s %-10s %-10s %-20s\n", $5, $2, $3, $4, $1)}''`
+        output=`./oc get pods | awk '/Running/ {print}' | awk 'BEGIN {printf("%-10s %-10s %-10s %-10s %-20s\n" ,"AGE", "READY", "STATUS", "RESTARTS", "NAME")} {printf("%-10s %-10s %-10s %-10s %-20s\n", $5, $2, $3, $4, $1)}'`
 
         echo $output
 
         curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$output\"}" https://hooks.slack.com/services/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
 fi
-
-
