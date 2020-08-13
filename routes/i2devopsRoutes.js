1bcfefffe3dc95dc2f82e4aae976c311d370498b
Vijesh V Nair <59731317+vijesh-v@users.noreply.github.com>
2020-08-13 15:33:48 +0530

    Update i2devopsRoutes.js

--- routes/i2devopsRoutes.js
+++ routes/i2devopsRoutes.js
@@ -14,7 +14,7 @@ router.post('/api/v1/ocgetpods', async (req, res) => {
 
 	logger.debug(req);
 		
-		res.status(200).send(req);
+		res.status(200).send(req.body.challenge);
 	}
 	catch (err) {
 		logger.error("Exception in Serving the Request "+err)
