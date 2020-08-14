42fe079062b2bc325e24eb4137a3dfe7c3345fa3
Vijesh V Nair <59731317+vijesh-v@users.noreply.github.com>
2020-08-14 10:53:39 +0530

    Update chatbot.sh

--- chatbot.sh
+++ chatbot.sh
@@ -1,25 +1,27 @@
 command="$1"
+slacktoken="$2"
+openshifttoken="$3"
 
 echo "Executing Command : $command"
 
 if [ "$command" == 'get pods' ]
 then
         echo "Executing OC Commands"
-        ./oc login --token=Ns9Q0zEUDSvDs-Guyp60uUIoEvNXUmOBMHeQv1IpSAE --server=https://c100-e.eu-gb.containers.cloud.ibm.com:30450
+        ./oc login --token=$openshifttoken --server=https://c100-e.eu-gb.containers.cloud.ibm.com:30450
 
         ./oc project i2devops
         output=`./oc get pods | awk '/Running/ {print}' | awk 'BEGIN {printf("%-10s %-10s %-10s %-10s %-20s\n" ,"AGE", "READY", "STATUS", "RESTARTS", "NAME")} {printf("%-10s %-10s %-10s %-10s %-20s\n", $5, $2, $3, $4, $1)}'`
 
         echo "Sending the Result to Slack"
 
-        curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$output\"}" https://hooks.slack.com/services/T018BGFM3M4/B018Z0L6EF6/$2
+        curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$output\"}" https://hooks.slack.com/services/T018BGFM3M4/B018Z0L6EF6/$slacktoken
 fi
 
 flag=`echo $command|awk '{print match($0,"delete pod")}'`;
 
 if [ $flag -gt 0 ];then
         echo "Executing OC Commands"
-        ./oc login --token=Ns9Q0zEUDSvDs-Guyp60uUIoEvNXUmOBMHeQv1IpSAE --server=https://c100-e.eu-gb.containers.cloud.ibm.com:30450
+        ./oc login --token=$openshifttoken --server=https://c100-e.eu-gb.containers.cloud.ibm.com:30450
 
         ./oc project i2devops
         output=`./oc $command 2>&1`
@@ -28,7 +30,7 @@ if [ $flag -gt 0 ];then
         sleep 5s
         echo "Sending the Result to Slack"
 
-        curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$output\"}" https://hooks.slack.com/services/T018BGFM3M4/B018Z0L6EF6/$2
+        curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$output\"}" https://hooks.slack.com/services/T018BGFM3M4/B018Z0L6EF6/$slacktoken
 else
     echo "fail";
 
